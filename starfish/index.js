bd844bb53aadf6c108cc82f7feb7b92ac31b32aa
Skyfall <mahadkalam1234@gmail.com>
2022-03-10 16:45:32 +0000

    add nicer error handling

--- starfish/index.js
+++ starfish/index.js
@@ -4,7 +4,9 @@ import "./component-wc.js";
 export async function fetchComponent(path, name) {
 	const response = await fetch(path);
 	if (response.status != 200)
-		throw new Error(`${path} load failed with error ${response.status}`);
+		throw new Error(
+			`[load ${name}] ${path} load failed with error ${response.status}`
+		);
 
 	const text = await response.text();
 	const template = parser.parseFromString(
@@ -19,27 +21,26 @@ export async function fetchComponent(path, name) {
 	);
 
 	const fragment = template.getElementsByTagName("template")[0].content;
-	fragment.querySelectorAll("script").forEach((s) => {
-		const element = document.createElement("script");
+	fragment.querySelectorAll("script").forEach((oldScript) => {
+		const newScript = document.createElement("script");
+		newScript.type = "module";
+		if (oldScript.src) newScript.src = oldScript.src;
+		newScript.innerHTML = oldScript.innerHTML;
 
-		// Force conventions
-		element.type = "module";
-		if (s.src) element.src = s.src;
-		element.text = s.text;
-
-		fragment.append(element);
+		oldScript.parentNode.replaceChild(newScript, oldScript);
 	});
 	fragment.querySelectorAll("component").forEach((component) => {
 		const replacement = document.createElement("component-", {});
-		const componentPath = component.getAttribute("path");
-		const componentName = component.getAttribute("name")
-		replacement.setAttribute("path", componentPath)
-		replacement.setAttribute("name", componentName)
+		const componentPath = component.getAttribute("src");
+		const componentName = component.getAttribute("name");
+
+		replacement.setAttribute("path", componentPath);
+		replacement.setAttribute("name", componentName);
 
 		// Persist contents
 		replacement.innerHTML = component.innerHTML;
 
-		component.parentNode.replaceChild(replacement, component)
+		component.parentNode.replaceChild(replacement, component);
 	});
 	return fragment;
 }
